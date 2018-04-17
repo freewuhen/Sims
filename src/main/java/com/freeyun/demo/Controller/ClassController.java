076c644f1b5f09a6390b03327a48d73e6090f6bb
freeyun <1571216237@qq.com>
2018-04-17 19:49:37 +0800

    added add_class function.

--- src/main/java/com/freeyun/demo/Controller/ClassController.java
+++ src/main/java/com/freeyun/demo/Controller/ClassController.java
@@ -1,17 +1,55 @@
 package com.freeyun.demo.Controller;
 
+import com.freeyun.demo.Domain.StudentClass;
+import com.freeyun.demo.Respository.StudentClassRespository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class ClassController {
+    @Autowired
+    private StudentClassRespository studentClassRespository;
+
     @GetMapping(value = {"/setclass","/setclass.html"})
-    String setclass()
+    String setClass(Model model)
     {
         SignVerification v = new SignVerification();
         if(v.Verification()) {
+            List<StudentClass> classes = studentClassRespository.findAll();
+            model.addAttribute("classes",classes);
             return "/setclass";
         }
         return "redirect:/signin";
+    }
+    @GetMapping(value = {"/addclass","/addclass.html"})
+    String addClass()
+    {
+        SignVerification v = new SignVerification();
+        if(v.Verification()) {
+        return "/addclass";
+        }
+        return "redirect:/signin";
+    }
+
+    @PostMapping(value = {"/addclass","/addclass.html"})
+    String addClass(StudentClass studentClass)
+    {
+        SignVerification v = new SignVerification();
+        if(v.Verification()) {
+            if (studentClassRespository.findDistinctByClassname(studentClass.getClassname()) != null) {
+                return "/error";
+            }
+
+            studentClassRespository.save(studentClass);
+
+            return "/ok";
+        }
+        return "redirect:/signin";
+
     }
 }
--- src/main/java/com/freeyun/demo/Domain/StudentClass.java
+++ src/main/java/com/freeyun/demo/Domain/StudentClass.java
@@ -1,13 +1,17 @@
 package com.freeyun.demo.Domain;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.io.Serializable;
+
 @Entity
-public class StudentClass {
+public class StudentClass  {
+    @GeneratedValue(strategy= GenerationType.SEQUENCE)
     @Id private Integer classno;
     private String classname;
     public StudentClass(){
-
     }
 
     public Integer getClassno() {
