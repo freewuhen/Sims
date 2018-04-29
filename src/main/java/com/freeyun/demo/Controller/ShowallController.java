017ddead8bcd8c4f83964a4eaf8e83e3da4c1ef8
freeyun <1571216237@qq.com>
2018-04-29 16:41:22 +0800

    Improve showall.html in the front and merge upadate the student's informition . but there are  some bugs when submitting a form repeatedly.

--- src/main/java/com/freeyun/demo/Controller/ShowallController.java
+++ src/main/java/com/freeyun/demo/Controller/ShowallController.java
@@ -1,20 +1,24 @@
 package com.freeyun.demo.Controller;
 
 import com.freeyun.demo.Domain.Student;
+import com.freeyun.demo.Domain.StudentClass;
+import com.freeyun.demo.Domain.Student_info;
 import com.freeyun.demo.Respository.StudentRespository;
 import com.freeyun.demo.Verification.SignVerification;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Controller
 public class ShowallController {
     @Autowired
     private StudentRespository studentRepository;
-    @GetMapping("/showall.html")
+    @GetMapping(value = {"/showall.html","/showall"})
     String GetStudentList(Model model){
         SignVerification v = new SignVerification();
         if(v.Verification()) {
@@ -25,6 +29,12 @@ public class ShowallController {
         return "redirect:/signin";
 
     }
+    @PostMapping("/showall.html")
+    public String Update(Student student){
 
+        studentRepository.save(student);
+
+        return "redirect:/showall";
+    }
 
 }
